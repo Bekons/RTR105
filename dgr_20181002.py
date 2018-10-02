f7ba0ede7f18323aae53a257e4f8775484327546
user <twiggymr@gmail.com>
2018-10-02 13:35:19 +0300

    20181002_13:33

---
+++ dgr_20181002.py
@@ -0,0 +1,233 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> print(133)
+133
+>>> print(Hello world)
+SyntaxError: invalid syntax
+>>> print('Helo World'
+      )
+Helo World
+>>> if = 2
+SyntaxError: invalid syntax
+>>> is=2
+SyntaxError: invalid syntax
+>>> a=2
+>>> a
+2
+>>> print('Ēart is flat'°
+      
+SyntaxError: invalid syntax
+>>> print('Ēart is flat')
+Ēart is flat
+>>> x=2
+>>> y=5
+>>> x
+2
+y
+>>> 
+>>> y
+5
+>>> x=100
+>>> x
+100
+>>> a=35
+>>> b=12.50
+>>> c=a*b
+>>> c
+437.5
+>>> print(c)
+437.5
+>>> help print
+SyntaxError: invalid syntax
+>>> x=2
+>>> x
+2
+>>> x=x+2
+>>> x
+4
+>>> print(x)
+4
+>>> x=3
+>>> x
+3
+>>> x=x+2
+>>> x
+5
+>>> x
+5
+>>> x=3.9*x*(1-x)
+>>> x
+-78.0
+>>> x=3.9*x*(1-x)	print(x)
+SyntaxError: invalid syntax
+>>> print(x)
+-78.0
+>>> x
+-78.0
+>>> xx
+
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    xx
+NameError: name 'xx' is not defined
+>>> 	xx=50
+	
+  File "<pyshell#39>", line 1
+    xx=50
+    ^
+IndentationError: unexpected indent
+>>> xx=0
+>>> xx
+0
+>>> xx=50
+>>> xx
+50
+>>> x-xx
+-128.0
+>>> x+xx
+-28.0
+>>> xx
+50
+x
+>>> 
+>>> x
+-78.0
+>>> xx
+50
+>>> type(x)
+<type 'float'>
+>>> x
+-78.0
+>>> xx
+50
+>>> type(x)
+<type 'float'>
+>>> type(xx)
+<type 'int'>
+>>> x
+-78.0
+>>> xx=float(x)
+>>> xx
+-78.0
+>>> xx=int(x)
+>>> xx
+-78
+>>> x
+-78.0
+>>> print(10/2)
+5
+>>> nam=input('Who are you?')
+Who are you?Marcis
+
+Traceback (most recent call last):
+  File "<pyshell#62>", line 1, in <module>
+    nam=input('Who are you?')
+  File "<string>", line 1, in <module>
+NameError: name 'Marcis' is not defined
+>>> nam=input('Who are you?')
+Who are you?Chuck
+
+Traceback (most recent call last):
+  File "<pyshell#63>", line 1, in <module>
+    nam=input('Who are you?')
+  File "<string>", line 1, in <module>
+NameError: name 'Chuck' is not defined
+>>> chuck=me
+
+Traceback (most recent call last):
+  File "<pyshell#64>", line 1, in <module>
+    chuck=me
+NameError: name 'me' is not defined
+>>> chuck
+
+Traceback (most recent call last):
+  File "<pyshell#65>", line 1, in <module>
+    chuck
+NameError: name 'chuck' is not defined
+>>> chuck=Me
+
+Traceback (most recent call last):
+  File "<pyshell#66>", line 1, in <module>
+    chuck=Me
+NameError: name 'Me' is not defined
+>>> x=me
+
+Traceback (most recent call last):
+  File "<pyshell#67>", line 1, in <module>
+    x=me
+NameError: name 'me' is not defined
+>>> x=1
+>>> x
+1
+>>> chuck='me'
+>>> #Get shit done
+>>> 
+>>> #Get it done
+>>> 
+>>> #things 	x=2
+>>> x
+1
+>>> #Get the name of the file and open it
+>>> name=input('Enter file') 	handle=open(name,'r')
+SyntaxError: invalid syntax
+>>> name=input('Enter file')
+Enter filemmm
+
+Traceback (most recent call last):
+  File "<pyshell#81>", line 1, in <module>
+    name=input('Enter file')
+  File "<string>", line 1, in <module>
+NameError: name 'mmm' is not defined
+>>> hours=input('How many hours?')
+How many hours?1
+>>> hours
+1
+>>> hours=input('How many hours?'
+	    rate=input('How many hours?'
+		       
+SyntaxError: invalid syntax
+>>> for line:
+	
+SyntaxError: invalid syntax
+>>> hours=input('how many hours?')
+how many hours?5
+>>> hours
+5
+>>> rate=input('what rate?')
+what rate?2
+>>> pay=hours*rate
+>>> pay
+10
+>>> pay=input(hours)*input(rate)
+5
+
+Traceback (most recent call last):
+  File "<pyshell#92>", line 1, in <module>
+    pay=input(hours)*input(rate)
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> pay=input('Hours spent?')*input('At what rate?')
+Hours spent?10
+At what rate?5
+>>> pay
+50
+>>> pay=input('Hours spent?')*input('At what rate?') print(pay)
+SyntaxError: invalid syntax
+>>> pay=input('Hours spent?')*input('At what rate?'):print(pay)
+SyntaxError: invalid syntax
+>>> pay=input('Hours spent?')*input('At what rate?') print(pay)
+SyntaxError: invalid syntax
+>>> pay
+50
+>>> pay=input('Hours spent?')*input('At what rate?') print(pay)
+SyntaxError: invalid syntax
+>>> pay=input('Hours spent?')*input('At what rate?')
+Hours spent?5
+At what rate?10
+>>> pay
+50
+>>> pay=input('Hours spent?')*input('At what rate?')input(pay)
+SyntaxError: invalid syntax
